1dbfeb9ca9b65ffa3a524b6cf990c2a44cf3510c
Bob Witmer <bwitmer@golden.net>
2023-10-02 16:26:29 -0400

    Initial Commit

---
+++ Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by Bob Witmer on 2023-10-02.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
